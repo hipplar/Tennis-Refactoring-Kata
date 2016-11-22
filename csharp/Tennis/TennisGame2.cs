bf8ab750dbd82280a27d077fccda1d7745c2f7d4
Robert Wilson <Robert Wilson>
2016-11-22 00:29:54 -0500

    Refactoring of Game2.

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Tennis
 {
     public class TennisGame2 : ITennisGame
@@ -5,137 +7,98 @@ namespace Tennis
         private int p1point;
         private int p2point;
 
-        private string p1res = "";
-        private string p2res = "";
+        private string player1Result = "";
+        private string player2Result = "";
         private string player1Name;
         private string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
             var score = "";
-            if (p1point == p2point && p1point < 3)
+            if (p1point == p2point)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
+                switch (this.p1point)
+                {
+                    case 0: score = "Love"; break;
+                    case 1: score = "Fifteen"; break;
+                    case 2: score = "Thirty"; break;
+                    default: score = "Deuce"; break;
+                }
+                if (p1point < 3)
+                {
+                    score += "-All";
+                }
             }
-            if (p1point == p2point && p1point > 2)
-                score = "Deuce";
+            else
+            {
+                switch (this.p1point)
+                {
+                    case 0: this.player1Result = "Love"; break;
+                    case 1: this.player1Result = "Fifteen"; break;
+                    case 2: this.player1Result = "Thirty"; break;
+                    case 3: this.player1Result = "Forty"; break;
+                    default: break;
+                }
+                switch (this.p2point)
+                {
+                    case 0: this.player2Result = "Love"; break;
+                    case 1: this.player2Result = "Fifteen"; break;
+                    case 2: this.player2Result = "Thirty"; break;
+                    case 3: this.player2Result = "Forty"; break;
+                    default: break;
+                }
 
-            if (p1point > 0 && p2point == 0)
-            {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
+                if (this.p1point <= 3 && this.p2point <= 3)
+                {
+                    score = $"{this.player1Result}-{this.player2Result}";
+                }
+                else
+                {
+                    int scoreDifference = this.p1point - this.p2point;
+                    
+                    if (p1point > p2point && p2point >= 3)
+                    {
+                        score = $"Advantage {this.player1Name}";
+                    }
 
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
+                    if (p2point > p1point && p1point >= 3)
+                    {
+                        score = $"Advantage {this.player2Name}";
+                    }
 
-                p1res = "Love";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
+                    if (p1point >= 4 && (p1point - p2point) >= 2)
+                    {
+                        score = $"Win for {this.player1Name}";
+                    }
+                    if (p2point >= 4 && (p2point - p1point) >= 2)
+                    {
+                        score = $"Win for {this.player2Name}";
+                    }
+                }
             }
             return score;
         }
 
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
-
         public void WonPoint(string player)
         {
-            if (player == "player1")
-                P1Score();
+            if (player == this.player1Name)
+            {
+                this.p1point++;
+            }
+            else if (player == this.player2Name)
+            {
+                this.p2point++;
+            }
             else
-                P2Score();
+            {
+                throw new ArgumentOutOfRangeException($"Unable to assign point. Invalid player name: {player}.");
+            }
         }
 
     }
